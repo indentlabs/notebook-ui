c4d3ae011cc1ae20f148dc7b9e43c598d462e8b0
Andrew Brown <drusepth@gmail.com>
2019-05-18 16:56:46 -0500

    cleanup

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,12 +1,8 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action }    from '@storybook/addon-actions';
-import { linkTo }    from '@storybook/addon-links';
+import { muiTheme } from 'storybook-addon-material-ui';
 
-import {muiTheme} from 'storybook-addon-material-ui';
-
-import { Welcome } from '@storybook/react/demo';
 // import Button from "../src/features/common/Button";
 import Button from '@material-ui/core/Button';
 
@@ -14,7 +10,7 @@ storiesOf("Button", module)
   .addDecorator(muiTheme())
   .add("primary button", () => (
     <Button color="primary" variant="contained">
-      Link
+      Cool button
     </Button>
   ))
   .add("disabled", () => <Button disabled="true" variant="contained">Disabled stuff</Button>);
